6d9fceb902c2425c2625106743d1ee939cd24a90
bystrovpavelgit <bystrov.pavelme@gmail.com>
2022-11-19 11:11:03 +0300

    changed config

--- webapp/config.py
+++ webapp/config.py
@@ -2,9 +2,11 @@
     Apache License 2.0 Copyright (c) 2022 Pavel Bystrov
     web-app configuration
 """
+import logging
 import os
 from datetime import timedelta
 
+logging.basicConfig(filename='webapp.log', level=logging.INFO)
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir,
                                                       "..",
@@ -31,5 +33,6 @@ TYPE_MAP = {"вторые блюда": 1,
 FILL_RECIPE = "Заполните Рецепт"
 INGREDIENTS_NUM = "Количество Ингредиентов"
 RECIPE = "Рецепт"
+CALC_MODEL = "Обновление модели"
 
 
