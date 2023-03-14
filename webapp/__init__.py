13d316f77108532edea451708e2a10915fab8ecf
bystrovpavelgit <bystrov.pavelme@gmail.com>
2023-03-14 19:50:03 +0300

    added error handlers

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,12 +5,18 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from werkzeug import exceptions
 from webapp.db import DB
 from webapp.user.models import User
 from webapp.calculate.views import blueprint as calc_blueprint
 from webapp.user.views import blueprint as user_blueprint
 from webapp.stat.views import blueprint as stat_blueprint
 from webapp.recommend.views import blueprint as recommend_blueprint
+
+
+class InsufficientStorage(exceptions.HTTPException):
+    code = 507
+    description = 'Not enough storage space.'
 
 
 def create_app():
@@ -27,6 +33,14 @@ def create_app():
     app.register_blueprint(calc_blueprint)
 
     migrate = Migrate(app, DB)
+
+    @app.errorhandler(exceptions.BadRequest)
+    def handle_bad_request(err):
+        return 'bad request!', 400
+
+    @app.errorhandler(InsufficientStorage)
+    def handle_507(err):
+        return 'Not enough storage space', 507
 
     @login_manager.user_loader
     def load_user(user_id):
